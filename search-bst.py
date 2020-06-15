76915050b99d707744cc0df1dd91dd491b17c1fa
Susmita Maitra <susmita.maitra@axisbank.com>
2020-06-15 23:34:15 +0530

    Added solution for 15-06-2020

---
+++ search-bst.py
@@ -0,0 +1,21 @@
+#search in a binary search tree
+#15-06-2020
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def searchBST(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if root == None or root.val == val:
+            return root
+        elif root.val < val:
+            return self.searchBST(root.right, val)
+        else:
+            return self.searchBST(root.left, val)
